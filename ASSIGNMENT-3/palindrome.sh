dca719d350ff9a5ad4886f5d305ba208a6232b12
bhagya128 <92773680+bhagya128@users.noreply.github.com>
2021-10-19 19:11:44 +0530

    Update palindrome.sh

--- ASSIGNMENT-3/palindrome.sh
+++ ASSIGNMENT-3/palindrome.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
-n=12321
-rev=$(echo $n | rev)
-if [ $n -eq $rev ]; then
-   echo "Number is palindrome"
-else
-   echo "Number is not palindrome"
+echo "Enter a string to be entered:"
+read str
+echo
+len=`echo $str | wc -c`
+len=`expr $len - 1`
+i=1
+j=`expr $len / 2`
+while test $i -le $j
+do
+k=`echo $str | cut -c $i`
+l=`echo $str | cut -c $len`
+if test $k != $l
+then
+echo "String is not palindrome"
+exit
 fi
+i=`expr $i + 1`
+len=`expr $len - 1`
+done
